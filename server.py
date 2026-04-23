444ca959af2ed08132cfcc2179d6df4ecd726f6f
Ileana Gonzalez Cabello <19220103+ileana-gonzalez@users.noreply.github.com>
2026-04-23 17:46:58 -0400

    Task 8. Run static code analysis

--- server.py
+++ server.py
@@ -20,8 +20,10 @@ def sent_analyzer():
     response = sentiment_analyzer(text_to_analyze)
     label = response["label"]
     score = response["score"]
+    if label is None:
+        return "Invalid input! Try again."
+     # Return a formatted string with the sentiment label and score
     return "The given text has been identified as {} with a score of {}.".format(label.split('_')[1], score)
-
 
 @app.route("/")
 def render_index_page():
@@ -31,7 +33,5 @@ def render_index_page():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    ''' This functions executes the flask app and deploys it on localhost:5000
-    '''
+    ''' This functions executes the flask app and deploys it on localhost:5000'''
     app.run(host="0.0.0.0", port=5000)
-
